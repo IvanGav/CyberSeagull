f5957d23a573a268622f89922b30d7dc7f5236dd
Drillgon200 <drillgon200@gmail.com>
2023-04-21 22:33:23 -0700

    Background

--- src/game.cpp
+++ src/game.cpp
@@ -22,27 +22,54 @@
 ********************************************************************************************/
 
 #include "raylib.h"
+#include <stdint.h>
 #ifdef _WIN32
 #pragma comment(lib, "winmm.lib")
 #endif
 
+const int screenWidth = 960;
+const int screenHeight = 540;
+const float screenWidthf = float(screenWidth);
+const float screenHeightf = float(screenHeight);
+
+void DrawRectangleNorm(float x, float y, float width, float height, Color color) {
+	DrawRectangle(x * screenWidthf, y * screenHeightf, width * screenWidthf, height * screenHeightf, color);
+}
+
+
+struct TypingBox {
+	static constexpr uint32_t cap = 31;
+	char data[cap + 1];
+	uint32_t pos;
+};
+
+Texture2D loginTex;
+TypingBox loginUser;
+TypingBox loginPass;
+
+void do_login() {
+	DrawTextureNPatch(loginTex, NPatchInfo{ Rectangle{0,0,1920,1080} }, Rectangle{ 0, 0, screenWidth, screenHeight }, Vector2{}, 0.0F, WHITE);
+	DrawRectangleNorm(0.3F, 0.4F, 0.1F, 0.1F, BLACK);
+}
+
 int main(void) {
-    const int screenWidth = 960;
-    const int screenHeight = 540;
-    
-    InitWindow(screenWidth, screenHeight, "Cyber Seagull");
-    SetWindowIcon(LoadImage("resources/icon.png"));
+	InitWindow(screenWidth, screenHeight, "Cyber Seagull");
+	SetWindowIcon(LoadImage("resources/icon.png"));
+	loginTex = LoadTexture("resources/seagulllogin.png");
 
-    SetTargetFPS(60);
+	SetTargetFPS(60);
 
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        
-        ClearBackground(RAYWHITE);
-        DrawText("More Seagulls.", 190, 200, 20, Color{ 0, 0, 0, 255 });
+	void (*currentScreen)(void) = do_login;
 
-        EndDrawing();
-    }
-    CloseWindow();
-    return 0;
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+		
+		ClearBackground(RAYWHITE);
+		currentScreen();
+		//DrawText("More Seagulls.", 190, 200, 12, Color{ 0, 0, 0, 255 });
+
+		EndDrawing();
+	}
+	CloseWindow();
+	return 0;
 }
