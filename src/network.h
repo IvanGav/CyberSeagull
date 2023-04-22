a3f744e7d88b6a7c616be438a79bb257e69cb5ad
Ryan Makela <ryan.makela2004@gmail.com>
2023-04-22 12:16:28 -0700

    Fleshed out map

--- src/network.h
+++ src/network.h
@@ -64,7 +64,18 @@ void build_network_graph() {
 	netNodes.push_back({ NET_NODE_TYPE_HOME_SERVER, 0, 0 });
 	netNodes.push_back({ NET_NODE_TYPE_SERVER, 200, 0 });
 	netNodes.push_back({ NET_NODE_TYPE_FIREWALL, 400, 0, FIREWALL_CHALLENGE_ADD_ONE });
-	netNodes.push_back({ NET_NODE_TYPE_IDEA_SERVER, 600, 0 });
+	netNodes.push_back({ NET_NODE_TYPE_SERVER, 600, 0 });
+	netNodes.push_back({ NET_NODE_TYPE_SERVER, 700, 100 });
+	netNodes.push_back({ NET_NODE_TYPE_FIREWALL, 800, 0, FIREWALL_CHALLENGE_FIZZBUZZ });
+	netNodes.push_back({ NET_NODE_TYPE_SERVER, 1000, 0 });
+	netNodes.push_back({ NET_NODE_TYPE_ROUTER, 1100, 100 });
+	netNodes.push_back({ NET_NODE_TYPE_FIREWALL, 1200, 0, FIREWALL_CHALLENGE_NODE_SEARCH });
+	netNodes.push_back({ NET_NODE_TYPE_SERVER, 1400, 0 });
+	netNodes.push_back({ NET_NODE_TYPE_ROUTER, 1500, 100 });
+	netNodes.push_back({ NET_NODE_TYPE_SERVER, 1400, 200 });
+	netNodes.push_back({ NET_NODE_TYPE_FIREWALL, 1600, 0, FIREWALL_CHALLENGE_NODE_AND_HEX2DEC });
+	netNodes.push_back({ NET_NODE_TYPE_SERVER, 1800, 0 });
+	netNodes.push_back({ NET_NODE_TYPE_IDEA_SERVER, 2000, 0 });
 
 	add_network_file(1, "README", "password");
 	add_network_file(3, "IDEA", "Make an idea game!");
@@ -72,4 +83,15 @@ void build_network_graph() {
 	connect_nodes(0, 1, 5);
 	connect_nodes(1, 2, 50);
 	connect_nodes(2, 3, 400);
+	connect_nodes(3, 4, 235);
+	connect_nodes(3, 5, 670);
+	connect_nodes(5, 6, 320);
+	connect_nodes(6, 7, 1104);
+	connect_nodes(6, 8, 1113);
+	connect_nodes(8, 9, 1216);
+	connect_nodes(9, 10, 1218);
+	connect_nodes(10, 11, 913);
+	connect_nodes(10, 12, 278);
+	connect_nodes(12, 13, 920);
+	connect_nodes(13, 14, 148);
 }
