d5d76c525e193b0dd1a02c9488871f4101302427
Drillgon200 <drillgon200@gmail.com>
2023-04-21 21:37:45 -0700

    Text rendering test

--- src/game.cpp
+++ src/game.cpp
@@ -22,46 +22,27 @@
 ********************************************************************************************/
 
 #include "raylib.h"
+#ifdef _WIN32
 #pragma comment(lib, "winmm.lib")
+#endif
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
 int main(void) {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 960;
+    const int screenHeight = 540;
+    
+    InitWindow(screenWidth, screenHeight, "Cyber Seagull");
+    SetWindowIcon(LoadImage("resources/icon.png"));
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    SetTargetFPS(60);
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
+    while (!WindowShouldClose()) {
         BeginDrawing();
-
+        
         ClearBackground(RAYWHITE);
-
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawText("Literally seagull.", 190, 200, 20, Color{ 0, 0, 0, 255 });
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
+    CloseWindow();
     return 0;
 }
